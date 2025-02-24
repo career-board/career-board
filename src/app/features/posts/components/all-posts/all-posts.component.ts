ef298e8408f109440e98625a51c8644eefaa92a0
Supun Kavinda <naskavinda@gmail.com>
2025-02-24 22:48:44 +0530

    Add loading skeleton

--- src/app/features/posts/components/all-posts/all-posts.component.ts
+++ src/app/features/posts/components/all-posts/all-posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
+import { SkeletonModule } from 'primeng/skeleton';
 import { PostComponent } from '../post/post.component';
 import { PostService } from '../../services/post.service';
 import { Post } from '../../models/post.model';
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-all-posts',
   standalone: true,
-  imports: [CommonModule, PaginatorModule, PostComponent],
+  imports: [CommonModule, PaginatorModule, SkeletonModule, PostComponent],
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.scss']
 })
@@ -18,6 +19,7 @@ export class AllPostsComponent implements OnInit {
   totalPosts: number = 0;
   pageSize: number = 10;
   pageIndex: number = 0;
+  loading: boolean = true;
 
   constructor(private postService: PostService, private router: Router) {}
 
@@ -26,13 +28,18 @@ export class AllPostsComponent implements OnInit {
   }
 
   loadPosts(): void {
+    this.loading = true;
     this.postService.getAllPosts(this.pageIndex, this.pageSize).subscribe({
       next: (response) => {
-        this.posts = response.content;
-        this.totalPosts = response.totalElements;
+        setTimeout(() => {  
+          this.posts = response.content;
+          this.totalPosts = response.totalElements;
+          this.loading = false;
+        }, 1000);
       },
       error: (error) => {
         console.error('Error loading posts:', error);
+        this.loading = false;
       }
     });
   }
--- src/app/features/posts/components/post/post.component.ts
+++ src/app/features/posts/components/post/post.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 import { Post } from '../../models/post.model';
 
 @Component({
@@ -9,10 +10,11 @@ import { Post } from '../../models/post.model';
   standalone: true,
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  imports: [CommonModule, CardModule],
+  imports: [CommonModule, CardModule, SkeletonModule],
 })
 export class PostComponent {
   @Input() post!: Post;
+  @Input() loading: boolean = true;
 
   constructor(private router: Router) {}
 

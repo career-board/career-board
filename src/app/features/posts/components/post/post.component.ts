b45f441c6d3bf0c2def5cdcd33e769381b41b8c9
Supun Kavinda <naskavinda@gmail.com>
2025-02-25 22:02:45 +0530

    used popup to show the interview details

--- src/app/features/posts/components/post/post.component.ts
+++ src/app/features/posts/components/post/post.component.ts
@@ -1,24 +1,58 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { SkeletonModule } from 'primeng/skeleton';
+import { DialogModule } from 'primeng/dialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Post } from '../../models/post.model';
+import { PostDetailsComponent } from '../details/post-details.component';
 
 @Component({
   selector: 'app-post',
   standalone: true,
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  imports: [CommonModule, CardModule, SkeletonModule],
+  imports: [CommonModule, CardModule, SkeletonModule, DialogModule],
+  providers: [DialogService]
 })
-export class PostComponent {
+export class PostComponent implements OnDestroy {
   @Input() post!: Post;
   @Input() loading: boolean = true;
+  
+  private ref: DynamicDialogRef | undefined;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private dialogService: DialogService
+  ) {}
 
   navigateToPost() {
-    this.router.navigate(['/dashboard/post', this.post.interviewId]);
+    // Show post details in dialog
+    this.ref = this.dialogService.open(PostDetailsComponent, {
+      header: 'Interview Details',
+      width: '70%',
+      height: '90vh',
+      maximizable: true,
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      style: {
+        'max-width': '1200px'
+      },
+      data: {
+        postId: this.post.interviewId
+      }
+    });
+
+    // Handle dialog close
+    this.ref.onClose.subscribe(() => {
+      this.ref = undefined;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.ref) {
+      this.ref.close();
+    }
   }
 }
